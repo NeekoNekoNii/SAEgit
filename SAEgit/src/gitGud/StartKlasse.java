e04baa6a9f82d80f7e458e6cf986278066c9eff9
Matthias Ott <matthias.ott@ausbildung-bbw.de>
2020-12-10 13:42:20 +0100

    first commit

---
+++ SAEgit/src/gitGud/StartKlasse.java
@@ -0,0 +1,15 @@
+package gitGud;
+
+public class StartKlasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("Hallo mein Name ist ");
+		}
+		
+	}
+
+}
