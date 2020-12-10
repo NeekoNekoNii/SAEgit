a4881958a3c0e509ef00fff6a5047ba5572050c3
Matthias Ott <matthias.ott@ausbildung-bbw.de>
2020-12-10 14:24:13 +0100

    Matthias Bearbeitung

--- SAEgit/src/gitGud/StartKlasse.java
+++ SAEgit/src/gitGud/StartKlasse.java
@@ -4,10 +4,10 @@ public class StartKlasse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String[] namen = {"Matthias"};
 		
 		for (int i = 0; i < args.length; i++) {
-			System.out.println("Hallo mein Name ist ");
+			System.out.println("Hallo mein Name ist "+name[i]);
 		}
 		
 	}
